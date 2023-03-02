adf0ee46a6fdbd2933453e25c22ba51a49f34977
Thompsonexe <thompsonsit77@gmail.com>
2023-03-02 18:17:50 +0800

    123

--- Test.js
+++ Test.js
@@ -1,36 +1,44 @@
 function main() {
 	// d3 code goes here
-	var data = [100, 400, 300, 900, 560, 1000]
-	var width = 500,
-	barHeight = 20
-	margin =1;
-
-	var scale = d3.scaleLinear()
-			.domain([d3.min(data), d3.max(data)])
-			.range([50, 500]);
+	var width = 800
+	var height = 800
+	var data = [10, 15, 20, 25, 30, 35]
+	var colors = ['#9C4F96', '#FF6355','#FBA949', '#FAE442', '#8BD448','#2AA8F2' ]
 	
-	var svg = d3.select('body').append('svg')
-			.attr('width', width)
-			.attr('height', barHeight * data.length);
+	var svg = d3.select('body')
+			.append('svg')
+			.attr('width',width )
+			.attr('height', height);
 	
 	var group = svg.selectAll('g')
 			.data(data)
-			.enter()
-			.append('g')
-			.attr('transform', function(d,i){
-				return 'translate(0,' + i * barHeight + ')';
-			});
-	group.append('rect')
-		.attr('width', function(d){
-			return scale(d);
-		})
-		.attr('height', barHeight - margin);
-	
+			.enter().append('g')
+			.attr('transform', function(d, i){
+				return 'translate(0,0)';
+			})
+	group.append('circle')
+		.attr('cx', function(d, i){
+				return i * 100 + 50;
+			})
+		.attr('cy', function(d,i){
+				return 100;
+			})
+		.attr('r', function(d){
+				return d * 1.5;
+			})
+		.attr('fill', function(d, i){
+				return colors[i];
+			})
 	group.append('text')
-		.attr('x', function (d) {
-			return (scale(d));
+		.attr('x', function (d,i) {
+			return i * 100 + 40;
 		})
-		.attr('y', barHeight / 2)
-		.attr('dy', '.35em')
-		.text(function(d){return d;});
+		.attr('y', 105)
+		.attr('stroke', 'black')
+		.attr('font-size', '20px')
+		.attr('font-family', 'sans-serif')
+		.text(function(d){
+			return d;
+		})
+
 }
